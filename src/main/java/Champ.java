46635bfdaab4763523ff4dd7e69e58922ec793a7
kevin <kevi@kevin.com>
2015-02-24 09:43:07 +0100

    class champ et tuple

---
+++ src/main/java/Champ.java
@@ -0,0 +1,27 @@
+package main.java;
+
+public class Champ {
+	private String valeur;
+	private String type;
+	
+	
+	public Champ(String valeur, String type) {
+		super();
+		this.valeur = valeur;
+		this.type = type;
+	}
+	
+	public String getValeur() {
+		return valeur;
+	}
+	public void setValeur(String valeur) {
+		this.valeur = valeur;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
--- src/main/java/TupleLinda.java
+++ src/main/java/TupleLinda.java
@@ -1,7 +1,22 @@
 package main.java;
 
+import java.util.ArrayList;
+
 
 public class TupleLinda {
 
+	ArrayList<Champ> monTuple = new ArrayList<Champ>();
+
+	public TupleLinda(String tupleLin) {
+		String champs[] = tupleLin.split(",");
+		for (String champ : champs){
+			String cham[] = champ.split(" ");
+			Champ field = new Champ(cham[0],cham[1]);
+			monTuple.add(field);
+		}
+		
+		
+	}
+	
 	
 }
