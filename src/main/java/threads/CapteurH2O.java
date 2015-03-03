98c16753662f81073ac26e8380cf8639b96ccbbb
lachandv <valentin.lachand@gmail.com>
2015-03-03 10:32:42 +0100

    Refactoring structure

---
+++ src/main/java/threads/CapteurH2O.java
@@ -0,0 +1,18 @@
+package threads;
+
+import linda.*;
+
+/**
+ * Created by valentin on 03/03/15.
+ */
+public class CapteurH2O extends Thread {
+
+    private EspaceTuple ts;
+
+    public CapteurH2O(EspaceTuple ts) {
+        this.ts = ts;
+    }
+    public void run() {
+
+    }
+}
